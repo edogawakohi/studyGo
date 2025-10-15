4a1b00db617b6f1dbfbf6ce9a74581714936e97f
Pham Quoc Anh Khoi <info.khoipham@gmail.com>
2025-10-15 22:06:16 +0700

    update about map Golang

--- main.go
+++ main.go
@@ -1,42 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Employee struct {
+	Name string
+	Age  int
+	Role string
+}
 
 func main() {
-	drink := map[string]int{
-		"coffe": 500,
-		"tea":   200,
+
+	employees := map[string]Employee{
+		"emp1": {"Khanh", 21, "Student"},
+		"emp2": {"Hien", 22, "Teacher"},
+		"emp3": {"Van", 27, "Developer"},
 	}
 
-	fmt.Printf("%+v", drink)
+	fmt.Println(employees)
 
-	student := map[int]string{
-		10: "Thanh",
-		9:  "Yen",
-		8:  "Khanh",
+	fmt.Printf("Name: %s \n", employees["emp1"].Name)
+	fmt.Printf("Age: %d \n", employees["emp1"].Age)
+	fmt.Printf("Role: %s \n", employees["emp1"].Role)
+
+	for _, value := range employees {
+		fmt.Printf("Name: %s | Age: %d | Role: %s\n", value.Name, value.Age, value.Role)
 	}
-	fmt.Printf("%+v", student)
 
-	//create map with make
+	studentSub := map[string][]string{
+		"Khoi": {"Golang", "HTML", "CSS", "JS"},
+		"Anh":  {"Toan", "Ly", "Hoa", "Su"},
+		"Tuan": {"Van", "Anh", "Sinh", "Cong Nghe"},
+	}
 
-	m := make(map[string]int)
+	for key, value := range studentSub {
+		for _, subject := range value {
+			fmt.Printf("Mon hoc cua %s la: %s \n", key, subject)
+		}
+		fmt.Printf("Mon hoc cua %s la: %s \n", key, value[0])
+	}
 
-	m["a"] = 1
-	m["b"] = 2
-	m["c"] = 3
-	m["d"] = 4
-	m["e"] = 5
+	// drink := map[string]int{
+	// 	"coffe": 500,
+	// 	"tea":   200,
+	// }
 
-	fmt.Println(m)
+	// fmt.Printf("%+v", drink)
 
-	var monAn map[string]int
+	// student := map[int]string{
+	// 	10: "Thanh",
+	// 	9:  "Yen",
+	// 	8:  "Khanh",
+	// }
 
-	monAn = make(map[string]int)
+	// for key, value := range student {
+	// 	fmt.Printf("Student[%d] is: %s\n", key, value)
+	// }
 
-	monAn["pho"] = 10
-	monAn["bun"] = 50
-	monAn["com"] = 30
-	monAn["banh mi"] = 20
+	// //create map with make
 
-	fmt.Printf("%+v \n", monAn)
+	// m := make(map[string]int)
+
+	// m["a"] = 1
+	// m["b"] = 2
+	// m["c"] = 3
+	// m["d"] = 4
+	// m["e"] = 5
+
+	// fmt.Println(m)
+
+	// var monAn map[string]int
+
+	// monAn = make(map[string]int)
+
+	// monAn["pho"] = 10
+	// monAn["bun"] = 50
+	// monAn["com"] = 30
+	// monAn["banh mi"] = 20
+
+	// fmt.Printf("%+v \n", monAn)
+
+	// value, exists := monAn["bun"]
+
+	// if exists {
+	// 	fmt.Println(value)
+	// } else {
+	// 	fmt.Println("Empty")
+	// }
+
 }
